c7dbdc6f8456ed5201f29823bb1abc3ef502205a
Raigato <gabriel.raymondou@gmail.com>
2021-12-04 20:49:02 +0100

    tuples sandbox

---
+++ src/tuples.rs
@@ -0,0 +1,12 @@
+// Tuples group together values of different types
+// Max 12 elements
+
+pub fn run() {
+  println!("===========================");
+  println!("Hello from the tuples.rs file");
+  println!("---------------------------");
+
+  let person: (&str, &str, i8) = ("Brad", "Mass", 37);
+
+  println!("{} is from {} and is {}", person.0, person.1, person.2);
+}
